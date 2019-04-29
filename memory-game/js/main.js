b1397187595a1f6aebb2dc36a3cfbb670cba0dd8
Kenneth <kenneth@kenneth-ofosu.lan>
2019-04-29 15:56:19 -0400

    finished version

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -23,27 +23,41 @@ var cards = [
   cardImage: "images/king-of-diamonds.png"
   }
 ];
+
+function createBoard() {
+  for(let i = 0; i < cards.length; i++) {
+   var cardElement = document.createElement('img');
+   cardElement.setAttribute('src', 'images/back.png');
+   cardElement.setAttribute('data-id', i);
+   cardElement.addEventListener('click', flipCard);
+   document.getElementById('game-board').appendChild(cardElement);
+  }
+}
+
 var cardsInPlay = [];
 
 function checkForMatch() {
   if(cardsInPlay[0] === cardsInPlay[1]) {
-    console.log("You found a match!");
+    alert("You found a match!");
   } else {
-    console.log("Sorry, try again.");
+    alert("Sorry, try again.");
   }
 }
 
-function flipCard(cardId) {
+function flipCard() {
+  var cardId = this.getAttribute("data-id");
+  this.setAttribute('src', cards[cardId].cardImage);
+  cardsInPlay.push(cards[cardId].rank);
   if(cardsInPlay.length === 2) {
     checkForMatch();
   };
 
 console.log(`User flipped ${cards[cardId].rank}.`);
 
-cardsInPlay.push(cards[cardId].rank);
+
 console.log(cards[cardId].cardImage);
 console.log(cards[cardId].suit);
 }
 
-flipCard(0);
-flipCard(2);
+createBoard();
+
